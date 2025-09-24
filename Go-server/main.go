8590004a604b960dd8706d49083c037419f46753
NasrulKarib <karibarian12@gmail.com>
2025-09-24 12:31:54 +0600

    feat: replace handleWebSocket logic as per coder/websocket

--- Go-server/main.go
+++ Go-server/main.go
@@ -1,66 +1,72 @@
 package main
 
-import(
+import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
+	"time"
+
+	"github.com/coder/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func (r *http.Request) bool{
-		return true;
-	},
-}
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request){
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil{
-		log.Printf("WebSocket upgrade failed: %v", err)
-        return
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		log.Printf("WebSocket accept failed: %v", err)
+		return
 	}
-	defer conn.Close()
+	defer conn.Close(websocket.StatusInternalError, "Server error")
+
 	log.Println("Client connected")
 
-	for{
-		_, message, err := conn.ReadMessage()
-		if err != nil{
+	ctx, cancel := context.WithTimeout(context.Background(), time.Hour*24)
+	defer cancel()
+
+	for {
+		// Read message from client
+		_, message, err := conn.Read(ctx)
+		if err != nil {
 			log.Printf("Read error: %v", err)
-            break
+			break
 		}
 
-		fmt.Printf("Client said: %s\n",string(message))
+		fmt.Printf("Client said: %s\n", string(message))
 
 		response := fmt.Sprintf("Server: %s", string(message))
-		err = conn.WriteMessage(websocket.TextMessage, []byte(response))
-		if err != nil{
+
+		// Write response back to client
+		err = conn.Write(ctx, websocket.MessageText, []byte(response))
+		if err != nil {
 			log.Printf("Write error: %v", err)
-            break
+			break
 		}
 	}
 
 	log.Println("Client disconnected")
-
+	conn.Close(websocket.StatusNormalClosure, "Connection closed")
 }
 
 func enableCORS(w http.ResponseWriter) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
+func homePage(w http.ResponseWriter, r *http.Request) {
 	enableCORS(w)
 	w.Write([]byte("Websocket server is running!"))
 }
 
-func main(){
+func main() {
 	// Route
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws",handleWebSocket)
+	http.HandleFunc("/ws", handleWebSocket)
 
 	fmt.Println("WebSocket server starting on :8080")
-    fmt.Println("WebSocket endpoint: ws://localhost:8080/ws")
-    
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("WebSocket endpoint: ws://localhost:8080/ws")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
